e59a18666198221b1ebb886a239926df734066b9
jacqueline ato <paolo_ato@dlsu.edu.ph>
2018-08-09 03:19:04 +0800

    Can make Meme Model (working on posting meme)

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose")
 const cookieparser = require("cookie-parser")
 const formidable = require('formidable'); 
 const fs = require('fs');
+const crypto = require("crypto"); 
+
 //const fs = require('file-system')//for files
 //const multer = require('multer') //for uploading files
 
@@ -35,14 +37,16 @@ const userSchema = mongoose.Schema({
     email: String,
     userBio: String,
 	meme: [{
-		memeID: String,
+//		memeID: String,
 		memeTitle: String,
-		memeTag: [{
-            tag:String
-            }],
+        memeimage: { type: String },
+        memeTag: String,
+//		memeTag: [{
+//            tag:String
+//            }],
 		memeOwner: String,
-		memeDate: String,
-        memeDateTime: Date,
+//		memeDate: String,
+//        memeDateTime: Date,
         memePrivacy:String,
 //		commentNumber: Number
 	}]
@@ -63,25 +67,21 @@ function addUser(username, password, image, email, userBio, callback){
     if(err) return console.error(err);
     callback();
   });
-
 }
 
-//function viewUsers(callback){
-//  userModel.find({}, function (err, list) {
-//    if(err) return console.error(err);
-//    callback(list);
-//  });
-//}
+
 
 const memeSchema = mongoose.Schema({
-    memeID: String,
+    //memeID: String,
 	memeTitle: String,
-	memeTag: [{
-    tag:String
-    }],
+    memeimage: { type: String },
+//	memeTag: [{
+//    tag:String
+//    }],
+    memeTag: String,
 	memeOwner: String,
-	memeDate: String,
-	memeDateTime: Date,
+//	memeDate: String,
+//	memeDateTime: Date,
     memePrivacy:String,
 //    memeShared:[{
 //        username:String
@@ -96,6 +96,22 @@ const memeSchema = mongoose.Schema({
 //	}]
 });
 const memeModel = mongoose.model('meme', memeSchema);
+
+function viewMeme(callback){
+  memeModel.find({}, function (err, list) {
+    if(err) return console.error(err);
+    callback(list);
+  });
+}
+
+function addMeme(memeTitle, memeimage, memeTag, memeOwner, memePrivacy, callback){
+  const instance = memeModel({ memeTitle: memeTitle, memeimage: memeimage, memeTag: memeTag, memeOwner: memeOwner, memePrivacy: memePrivacy });
+   console.log("test");
+  instance.save(function (err, inv) {
+    if(err) return console.error(err);
+    callback();
+  });
+}
 
 server.use(session({
   name: 'User Session',
@@ -154,13 +170,19 @@ server.post('/main-postLogin', urlencoder,function(req, resp){
   }
     else{
     console.log(req.session.username);
-    const searchQuery = { username: req.body.username, password: req.body.password };
+    const searchQuery = { username: req.body.username};
+    var passcheck = req.body.password;
+    var hashedcheck = crypto.createHash("md5").update(passcheck).digest("hex");
+    console.log("Input: " + hashedcheck);
     userModel.findOne(searchQuery, function (err, foundUser) {
       if(err) return console.error(err);
         console.log(foundUser);
       if(foundUser != undefined && foundUser.username != null){
-        req.session.username = foundUser.username;
-        resp.render('./pages/main-postLogin',{username: req.session.username,image: foundUser.image});
+           console.log("DB: " + foundUser.password);
+          if(hashedcheck === foundUser.password){
+              req.session.username = foundUser.username;
+              resp.render('./pages/main-postLogin',{username: req.session.username,image: foundUser.image});
+          }
       }else
         resp.render('./pages/main-page');
     });
@@ -174,25 +196,67 @@ server.get('/sign-up', function(req, resp){
 server.post('/main-page', function(req, resp){
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
+      var hashedpassword = crypto.createHash("md5").update(fields.password).digest("hex");
       var oldpath = files.image.path;
       var newpath = __dirname + '\\public\\new\\' + files.image.name;
       fs.rename(oldpath, newpath, function (err) {
         console.log('Saving files to new folder');
         if (err) throw err;
-        addUser(fields.username, fields.password ,files.image.name, fields.email , fields.userBio ,function(){ 
+        addUser(fields.username, hashedpassword ,files.image.name, fields.email , fields.userBio ,function(){ 
           resp.render('./pages/main-page');
         });//adduser to DB
-      });//rename
-    });//parse
-  });//post
+      });
+    });
+  });
 
 server.get('/user-profile', urlencoder, function(req, resp){
     const searchUser = { username: req.session.username};
     userModel.findOne(searchUser, function (err, foundUser) {
          console.log("Object: " + foundUser);
-        resp.render('./pages/user-profile',{username: req.session.username, image: foundUser.image, userBio: foundUser.userBio});
+//             viewUsers(function(list){
+//            const data = { list:list };
+            resp.render('./pages/user-profile',{username: req.session.username, image: foundUser.image, userBio: foundUser.userBio});//, data:data
+//            });
     });
 });
+
+server.post('/user-profile', function(req, resp){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.memeimage.path;
+      var newpath = __dirname + '\\public\\memes\\' + files.memeimage.name;
+      fs.rename(oldpath, newpath, function (err) {
+        console.log('Saving files to new folder');
+        if (err) throw err;
+        const searchUser = { username: req.session.username};
+            userModel.findOne(searchUser, function (err, foundUser) {
+            addMeme(fields.memeTitle,files.memeimage.name, fields.memeTag ,foundUser.username , fields.memePrivacy ,function(){ 
+            console.log(fields.memeTitle);
+            console.log(files.memeimage.name);
+            console.log(fields.memeTag );
+            console.log(foundUser.username );
+            console.log(fields.memePrivacy);
+            //console.log("Object: " + foundUser);
+            var newmeme = new memeModel({
+                memeTitle: fields.memeTitle,
+                memeimage:files.memeimage.name,
+                memeTag: fields.memeTag,
+                memePrivacy: fields.memePrivacy
+            })
+            foundUser.meme.push(newmeme);
+//            viewUsers(function(list){
+//            const data = { list:list };
+            resp.render('./pages/user-profile',{username: req.session.username, image: foundUser.image, userBio: foundUser.userBio});//, data:data
+//            });
+            });
+        });//addmeme to DB
+      });
+    });
+  });
+//    viewUsers(function(list){
+//      const data = { list:list };
+//      resp.render('./pages/home',{ data:data });
+//    });
 
 server.get('/main-postLogin', function(req, resp){
    resp.render('./pages/main-postLogin');
